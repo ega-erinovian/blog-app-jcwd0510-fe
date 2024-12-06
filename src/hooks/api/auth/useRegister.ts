cdb38dd210cb6c5a0cb46789ad4270583ffcd4aa
ega-erinovian <erinovian69@gmail.com>
2024-12-06 16:52:25 +0700

    feat: register

---
+++ src/hooks/api/auth/useRegister.ts
@@ -0,0 +1,48 @@
+"use client";
+
+import { axiosInstance } from "@/lib/axios";
+import { useMutation } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+import { toast } from "react-toastify";
+
+// Use React Query
+interface RegisterPayload {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const useRegister = () => {
+  return useMutation({
+    mutationFn: async (payload: RegisterPayload) => {
+      const { data } = await axiosInstance.post("/auth/register", payload);
+      return data;
+    },
+    onSuccess: () => {
+      toast.success("Register Success");
+    },
+    onError: (error: AxiosError<any>) => {
+      toast.error(error.response?.data);
+    },
+  });
+};
+
+export default useRegister;
+
+// const useRegister = () => {
+//     const [isLoading, setIsLoading] = useState<boolean>(false);
+//     const handleRegister = async (payload) => {
+//       try {
+//         setIsLoading(true);
+//         const { data } = await axios.post("http://localhost:8000", payload);
+//         toast.success("Register Success");
+//       } catch (error) {
+//         console.log(error);
+//       } finally {
+//         setIsLoading(false);
+//       }
+//     };
+//     return { handleRegister, isLoading };
+// };
+
+// export default useRegister;
