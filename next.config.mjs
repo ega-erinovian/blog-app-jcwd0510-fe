d9c7ae6547ba019c9ccdcb59819f612aee985f11
ega-erinovian <erinovian69@gmail.com>
2024-12-10 15:40:56 +0700

    feat: add blog list

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "mclaren.scene7.com",
+      },
+      {
+        protocol: "https",
+        hostname: "www.gameskinny.com",
+      },
+      {
+        protocol: "https",
+        hostname: "akcdn.detik.net.id",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
