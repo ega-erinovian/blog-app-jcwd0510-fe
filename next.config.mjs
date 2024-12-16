b014e531f3ffa3e8f13ad0aa862067bf9f17ccac
Ega Erinovian <72180780+ega-erinovian@users.noreply.github.com>
2024-12-16 09:42:03 +0700

    feat: add create blog (#8)

--- next.config.mjs
+++ next.config.mjs
@@ -18,6 +18,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "akcdn.detik.net.id",
       },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
     ],
   },
 };
