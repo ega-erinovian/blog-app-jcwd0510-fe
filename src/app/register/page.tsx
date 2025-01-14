fd3a155909030afedc41e9b81bf61a75467cde67
Ega Erinovian <72180780+ega-erinovian@users.noreply.github.com>
2025-01-14 11:46:14 +0700

    feat: add metadata (#16)

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,14 @@
 import RegisterPage from "@/features/register";
 import { auth } from "@/lib/auth";
+import { Metadata } from "next";
 import { redirect } from "next/navigation";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: "Register | BlogGo",
+  description:
+    "Discover insightful articles and engaging stories on BlogGo. Explore a diverse range of topics, from tech and lifestyle to education and more. Stay informed and inspired—your go-to platform for quality content. Start reading today!",
+};
 
 const Register = async () => {
   const session = await auth();
